b59b3110c2aae7532a8886653884868fe0a66b91
AlfredoPegueroC <alfredocapellan20@gmail.com>
2024-08-04 20:34:21 -0400

    done

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const btn = document.getElementById("btn");
 const links = document.querySelector(".navbar_links");
+const navbar = document.querySelector("#id_navbar");
 
 btn.addEventListener("click", (e) => {
   links.classList.toggle("active");
+  navbar.classList.toggle("active");
+  
+  if (btn.attributes.src.textContent== "./images/icon-hamburger.svg") {
+    btn.src = "./images/icon-close.svg";
+  } else {
+    btn.src = "./images/icon-hamburger.svg";
+  }
+
 });
